dadffd23d98c1cb3844c6018288fe8f1b05235e1
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-18 19:20:43 -0300

    Create cards_home.dart

---
+++ lib/widgets/cards_home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CardsHome extends StatefulWidget {
+  const CardsHome({super.key});
+
+  @override
+  _SelectableCardState createState() => _SelectableCardState();
+}
+
+class _SelectableCardState extends State<CardsHome> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+        });
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(2),
+          side: BorderSide(
+            color:
+                isSelected ? const Color(0xFFE1E0D7) : const Color(0xFFFEDE6B),
+            width: 1.0,
+          ),
+        ),
+        color: Colors
+            .transparent, // Cor do Card condicionalmente com base no estado
+        child: Column(
+          children: [Text("aaaaaaaaaaaaaaaaa")],
+        ),
+      ),
+    );
+  }
+}
