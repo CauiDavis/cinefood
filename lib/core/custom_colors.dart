31812c954fcbdd28a9ffe8a21340c9e1723665cd
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-19 11:10:29 -0300

    Create custom_colors.dart

---
+++ lib/core/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color primaryColor = Color(0xFF170D0D);
+  static const Color secondaryColor = Color(0xFFE1E0D7);
+  static const Color tertiaryColor = Color(0xFF432727);
+  static const Color quaternaryColor = Color(0xFFFEDE6B);
+}
