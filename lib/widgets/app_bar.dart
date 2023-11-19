bcf539a9f52cd15262468e28bf63269747fe750a
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-19 20:55:44 -0300

    Update app_bar.dart

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar {
-  static final AppBar appBar = AppBar(
-          actions: [
-            Container(
-              margin: const EdgeInsets.only(left: 20),
-              child: Image.asset(
-                'assets/appbarIcon.png',
-                width: 110,
-                height: 30,
-              ),
-            ),
-            const SizedBox(
-              width: 180,
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 20),
-              child: IconButton(
-                onPressed: () => {},
-                icon: Image.asset('assets/exitIcon.png', width: 30, height: 30),
-              ),
-            )
-          ],
-        );
+  final VoidCallback? onPressed;
+
+  CustomAppBar({this.onPressed});
+
+  AppBar buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(left: 20),
+          child: Image.asset(
+            'assets/appbarIcon.png',
+            width: 110,
+            height: 30,
+          ),
+        ),
+        const SizedBox(
+          width: 180,
+        ),
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: IconButton(
+            onPressed: onPressed,
+            icon: Image.asset('assets/exitIcon.png', width: 30, height: 30),
+          ),
+        )
+      ],
+    );
+  }
 }
