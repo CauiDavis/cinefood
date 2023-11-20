e79b526ed0a18e87490d9f3fd054bdcc99d78a39
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-20 17:09:22 -0300

    Create history_client.dart

---
+++ lib/screens/history_client.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HistoryClient extends StatelessWidget {
+  const HistoryClient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Text('Historico'),
+    );
+  }
+}
