a22ec7ad66c9491c8bdeefa57a6e950a04493185
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-20 17:09:19 -0300

    Create favortes_client.dart

---
+++ lib/screens/favortes_client.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavoritesClient extends StatelessWidget {
+  const FavoritesClient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body:  Text('Favorites'),
+    );
+  }
+}
+
