eea8dfca54fa9ef0fcb48a78e25074ad8c2267a7
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-23 10:33:31 -0300

    Update favortes_client.dart

--- lib/screens/favortes_client.dart
+++ lib/screens/favortes_client.dart
@@ -1,4 +1,7 @@
+import 'package:cinefood/widgets/cards_home.dart';
 import 'package:flutter/material.dart';
+
+import '../core/custom_colors.dart';
 
 class FavoritesClient extends StatelessWidget {
   const FavoritesClient({super.key});
@@ -6,9 +9,41 @@ class FavoritesClient extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      body:  Text('Favorites'),
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Favoritos',
+                  style: TextStyle(
+                    color: CustomColors.quaternaryColor,
+                    fontSize: 24,
+                  )),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Divider(
+                  color: CustomColors.quaternaryColor,
+                  thickness: 1,
+                ),
+              ),
+              Expanded(
+                  child: SizedBox.expand(
+                      child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.2,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return CardsHome();
+                },
+              ))),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
-
