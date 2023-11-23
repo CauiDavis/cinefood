d9cea0fc16ed4cc1960abc162bc4ad9def8d3ff6
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-23 10:33:37 -0300

    Update home_client.dart

--- lib/screens/home_client.dart
+++ lib/screens/home_client.dart
@@ -3,70 +3,60 @@ import 'package:flutter/material.dart';
 import '../widgets/cards_home.dart';
 
 class HomeClient extends StatelessWidget {
-  const HomeClient({super.key});
+  HomeClient({super.key});
+  bool isCardSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Center(
-              child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 26),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Comida',
-                style: TextStyle(
-                  color: Color(0xFFFEDE6B),
-                  fontSize: 24,
-                )),
-            Expanded(
-                child: SizedBox.expand(
-                    child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-               
-                childAspectRatio: 1.2,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Comida',
+                  style: TextStyle(
+                    color: Color(0xFFFEDE6B),
+                    fontSize: 24,
+                  )),
+              Expanded(
+                  child: SizedBox.expand(
+                      child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.2,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return CardsHome();
+                },
+              ))),
+              SizedBox(
+                height: 10,
               ),
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return CardsHome();
-              },
-            ))),
-            SizedBox(
-              height: 40,
-            ),
-            SizedBox(
-                width: MediaQuery.of(context).size.width * 0.95,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Bebida',
-                      style: TextStyle(
-                        color: Color(0xFFFEDE6B),
-                        fontSize: 24,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CardsHome(),
-                        CardsHome(),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        CardsHome(),
-                        CardsHome(),
-                      ],
-                    )
-                  ],
-                )),
-          ],
+              Text('Bebida',
+                  style: TextStyle(
+                    color: Color(0xFFFEDE6B),
+                    fontSize: 24,
+                  )),
+              Expanded(
+                  child: SizedBox.expand(
+                      child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.2,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return CardsHome();
+                },
+              ))),
+            ],
+          ),
         ),
-      ))),
+      )),
     );
   }
 }
