b310921c80dadadabfe8992ed37aca4a04903b86
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-11-25 10:53:38 -0300

    Create request_admin.dart

---
+++ lib/screens/admin/request_admin.dart
@@ -0,0 +1,119 @@
+import 'package:cinefood/core/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../widgets/cards_request.dart';
+
+class RequestAdmin extends StatelessWidget {
+  const RequestAdmin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40, right: 26, left: 26),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pedidos',
+                  style: TextStyle(
+                    color: CustomColors.quaternaryColor,
+                    fontSize: 24,
+                  )),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Divider(
+                  color: CustomColors.quaternaryColor,
+                  thickness: 1,
+                ),
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    CardsRequest(
+                      pedido: Pedido(
+                        dataPedido: '23/11/2023',
+                        produtos: ['Produto 1', 'Produto 2', 'Produto 3'],
+                        statusPedido: 'Em Processo',
+                      ),
+                    ),
+                    CardsRequest(
+                      pedido: Pedido(
+                        dataPedido: '23/11/2023',
+                        produtos: ['Produto 1', 'Produto 2', 'Produto 3'],
+                        statusPedido: 'Em Processo',
+                      ),
+                    ),
+                    CardsRequest(
+                      pedido: Pedido(
+                        dataPedido: '23/11/2023',
+                        produtos: ['Produto 1', 'Produto 2', 'Produto 3'],
+                        statusPedido: 'Em Processo',
+                      ),
+                    ),
+                    CardsRequest(
+                      pedido: Pedido(
+                        dataPedido: '23/11/2023',
+                        produtos: ['Produto 1', 'Produto 2', 'Produto 3'],
+                        statusPedido: 'Em Processo',
+                      ),
+                    ),
+
+                    // Adicione mais itens conforme necessário
+                  ],
+                ),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    maximumSize: const Size(120, 40),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Concluir",
+                        style: TextStyle(
+                          color: CustomColors.secondaryColor,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.offline_pin,
+                        color: CustomColors.secondaryColor,
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    maximumSize: const Size(120, 40),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Excluir",
+                        style: TextStyle(
+                          color: CustomColors.secondaryColor,
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Icon(
+                        Icons.delete,
+                        color: CustomColors.secondaryColor,
+                      ),
+                    ],
+                  ),
+                ),
+              ])
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
