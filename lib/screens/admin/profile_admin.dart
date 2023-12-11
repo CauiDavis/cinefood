62eba437e3439afae64798c47fe7ce155f06c167
Cauí Davis Matos de Oliveira <98659275+CauiDavis@users.noreply.github.com>
2023-12-10 21:20:07 -0300

    Update profile_admin.dart

--- lib/screens/admin/profile_admin.dart
+++ lib/screens/admin/profile_admin.dart
@@ -1,13 +1,28 @@
-
 import 'package:flutter/material.dart';
 
 import '../../core/custom_colors.dart';
 
 class ProfileAdmin extends StatelessWidget {
-  const ProfileAdmin({super.key});
+  final String userPhotoUrl;
+  final String userName;
+
+  const ProfileAdmin({
+    required this.userPhotoUrl,
+    required this.userName,
+  });
+
+  String getFirstTwoNames(String fullName) {
+    List<String> names = fullName.split(' ');
+    if (names.length >= 2) {
+      return '${names[0]} ${names[1]}';
+    } else {
+      return fullName;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    String firstTwoNames = getFirstTwoNames(userName);
     return Scaffold(
       body: SafeArea(
           child: Center(
@@ -26,12 +41,12 @@ class ProfileAdmin extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.only(right: 10, top: 10),
                     child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/avatar2.png'),
+                      backgroundImage: NetworkImage(userPhotoUrl),
                       radius: 50,
                       backgroundColor: CustomColors.quaternaryColor,
                     ),
                   ),
-                  Text('Valeria',
+                  Text(firstTwoNames,
                       style: TextStyle(
                         color: CustomColors.quaternaryColor,
                         fontSize: 20,
