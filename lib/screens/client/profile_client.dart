17b93834c953f7055ff3fdb9e27a4ff993cf9777
Cauí Davis Matos de Oliveira <98659275+CauiDavis@users.noreply.github.com>
2023-12-16 17:25:48 -0300

    Update profile_client.dart

--- lib/screens/client/profile_client.dart
+++ lib/screens/client/profile_client.dart
@@ -1,6 +1,6 @@
 import 'package:cinefood/widgets/cards/cards_request.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../core/custom_colors.dart';
 
 class ProfileClient extends StatelessWidget {
@@ -24,92 +24,102 @@ class ProfileClient extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String firstTwoNames = getFirstTwoNames(userName);
-    
+
     return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 26),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Perfil',
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 26),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Perfil',
                   style: TextStyle(
                     color: CustomColors.quaternaryColor,
                     fontSize: 24,
-                  )),
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 10, top: 10),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(userPhotoUrl),
-                      radius: 50,
-                      backgroundColor: CustomColors.quaternaryColor,
-                    ),
                   ),
-                  Text(firstTwoNames,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 10, top: 10),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(userPhotoUrl),
+                        radius: 50,
+                        backgroundColor: CustomColors.quaternaryColor,
+                      ),
+                    ),
+                    Text(
+                      firstTwoNames,
                       style: TextStyle(
                         color: CustomColors.quaternaryColor,
                         fontSize: 20,
-                      )),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Divider(
-                  color: CustomColors.quaternaryColor,
-                  thickness: 1,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Text('Meus pedidos',
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Divider(
+                    color: CustomColors.quaternaryColor,
+                    thickness: 1,
+                  ),
+                ),
+                Text(
+                  'Meus pedidos',
                   style: TextStyle(
                     color: CustomColors.quaternaryColor,
                     fontSize: 20,
-                  )),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: ListView(
-                  children: [
-                    CardsRequest(
-                      request: Request(
-                        dataRequest: '23/11/2023',
-                        products: ['Produto 1', 'Produto 2', 'Produto 3'],
-                        statusRequest: 'Em Processo',
-                      ),
-                    ),
-                    CardsRequest(
-                      request: Request(
-                        dataRequest: '23/11/2023',
-                        products: ['Produto 1', 'Produto 2', 'Produto 3'],
-                        statusRequest: 'Em Processo',
-                      ),
-                    ),
-                    CardsRequest(
-                      request: Request(
-                        dataRequest: '23/11/2023',
-                        products: ['Produto 1', 'Produto 2', 'Produto 3'],
-                        statusRequest: 'Em Processo',
-                      ),
-                    ),
-                    CardsRequest(
-                      request: Request(
-                        dataRequest: '23/11/2023',
-                        products: ['Produto 1', 'Produto 2', 'Produto 3'],
-                        statusRequest: 'Em Processo',
-                      ),
-                    ),
-
-                    // Adicione mais itens conforme necessário
-                  ],
+                  ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('pedidos')
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text('Erro: ${snapshot.error}'),
+                        );
+                      }
+
+                      // Use os documentos reais do Firestore no lugar dos CardsRequest
+                      return ListView(
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                              document.data() as Map<String, dynamic>;
+
+                          // Substitua os dados fictícios pelos dados reais do Firestore
+                          return CardsRequest(
+                            request: Request(
+                              dataRequest: data['data'],
+                              products: List<String>.from(data['pedidos']),
+                              statusRequest: data['status'], 
+                            ), isAdmin: false,
+
+                          );
+                        }).toList(),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
